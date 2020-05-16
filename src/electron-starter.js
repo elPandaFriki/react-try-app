34e19c2fcb911c6320e8f732d71b317306467924
elPandaFriki <davidpemo36@hotmail.com>
2020-05-16 13:11:22 +0200

    Added electron for desktop web_app

---
+++ src/electron-starter.js
@@ -0,0 +1,38 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+  mainWindow.loadURL(startUrl);
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
